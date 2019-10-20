48c3d00656775d55a60b5ac5813a1c0137ff0f9e
solar05 <artem00298@gmail.com>
2019-10-20 16:31:30 +0300

    Add task statuses and seeder

---
+++ database/seeds/TaskStatusSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TaskStatusSeeder extends Seeder
+{
+    protected $statuses = ['created', 'in_work', 'testing', 'completed'];
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        foreach ($this->statuses as $status) {
+            $dbStatusRecord = new \App\TaskStatus();
+            $dbStatusRecord->name = $status;
+            $dbStatusRecord->save();
+        }
+    }
+}
