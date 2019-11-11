96fa2ab4a76149a309b415d83daccdafe4b0b234
solar05 <artem00298@gmail.com>
2019-11-11 11:58:17 +0300

    Forgot about status auth

--- app/Http/Controllers/TaskStatusController.php
+++ app/Http/Controllers/TaskStatusController.php
@@ -10,6 +10,11 @@ use Illuminate\Support\Facades\Validator;
 
 class TaskStatusController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+    
     /**
      * Display a listing of the resource.
      *
